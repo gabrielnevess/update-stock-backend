aff8a879cc8707bde4b028f8a5411099fffa6ed3
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-11-11 21:48:06 -0300

    first commit

---
+++ src/main/java/com/updatestock/updatestock/UpdateStockApplication.java
@@ -0,0 +1,13 @@
+package com.updatestock.updatestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/updatestock/updatestock/UpdateStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updatestock.updatestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
